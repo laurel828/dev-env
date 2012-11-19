5cbe84ae8c76389520f74a50a60c04de9c0ce9a1
pirates <laurel828@gmail.com>
2012-11-20 00:27:04 +0900

    test day of week

--- boost/dateGMT.cpp
+++ boost/dateGMT.cpp
@@ -16,11 +16,27 @@ int main()
     date         sToday(day_clock::local_day());
     stringstream sMidNightGMT;
 
+    date d1(2002,Jan,10);
+    date d2(2002,Jan,11);
+    date d3(2002,Jan,12);
+    date d4(2002,Jan,13);
+    date d5(2002,Jan,14);
+    date d6(2002,Jan,15);
+    date d7(2002,Jan,16);
+
+    cout << d1.day_of_week() << ", "
+         << d2.day_of_week() << ", "
+         << d3.day_of_week() << ", "
+         << d4.day_of_week() << ", "
+         << d5.day_of_week() << ", "
+         << d6.day_of_week() << ", "
+         << d7.day_of_week() << endl;
+
     sMidNightGMT << sToday.day_of_week() << ", " << sToday.day() << " "
                  << sToday.month() << " " << sToday.year() << " "
                  << "15:00:00 GMT";
 
-    cout << sMidNightGMT.str() << endl;
+    cout << sMidNightGMT.str() << endl << endl;
 
     ptime now = second_clock::local_time();
     ptime tomorrow(now.date() + date_duration(1));
